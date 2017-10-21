50e58dc7043285701acd8df2c9f8eb4ab9df05f0
zmj <zach.jeyakaran@gmail.com>
2017-10-21 15:53:11 -0400

    upload requests

--- upload/apimodels.go
+++ upload/apimodels.go
@@ -1,5 +1,28 @@
 package upload
 
 type folder struct {
-	ID string `json:"Id"`
+	Name string
+	ID   string `json:"Id"`
+}
+
+type uploadSpecReq struct {
+	Method   string
+	Raw      bool
+	FileName string
+	FileSize uint64
+}
+
+type uploadSpec struct {
+	ChunkURI string `json:"ChunkUri"`
+}
+
+type uploadResult struct {
+	Error  bool   `json:"error"`
+	ErrMsg string // check this
+	Value  file
+}
+
+type file struct {
+	ID  string `json:"id"`
+	Md5 string `json:"md5"`
 }
--- upload/uploader.go
+++ upload/uploader.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/cookiejar"
@@ -39,13 +40,52 @@ type uploader struct {
 }
 
 func (u *uploader) CreateFile(ctx context.Context, parentSfID, name string, content Content) (string, error) {
+	defer func() {
+		for range content.Bytes {
+		}
+	}()
 	if u == nil {
 		return "", fmt.Errorf("Uploader not initialized")
 	}
-	for b := range content.Bytes {
-		fmt.Printf("Fake upload %v\n%v", name, string(b))
+	url := fmt.Sprintf("https://%v/sf/v3/Items(%v)/Upload2")
+	usr := uploadSpecReq{
+		Method:   "Standard",
+		Raw:      true,
+		FileName: name,
+		FileSize: content.Size,
 	}
-	return "fi123", nil
+	var us uploadSpec
+	err := u.doApiPost(url, &usr, &us)
+	if err != nil {
+		return "", fmt.Errorf("Upload API call failed: %v", err)
+	}
+
+	req, err := http.NewRequest("POST", us.ChunkURI, content.reader())
+	if err != nil {
+		return "", fmt.Errorf("Create upload httpReq failed: %v", err)
+	}
+	req.Header.Add("Content-Type", "application/octet-stream")
+	resp, err := u.client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("Upload request failed: %v", err)
+	}
+	defer resp.Body.Close()
+	respBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("Failed to read upload response: %v", err)
+	}
+	var ur uploadResult
+	err = json.Unmarshal(respBytes, &ur)
+	if err != nil {
+		return "", fmt.Errorf("Failed to parse response: %v", err)
+	}
+	if ur.Error {
+		return "", fmt.Errorf("Upload server error: %v", ur.ErrMsg)
+	}
+	if ur.Value.ID == "" {
+		return "", fmt.Errorf("Upload response does not contain id")
+	}
+	return ur.Value.ID, nil
 }
 
 func (u *uploader) CreateFolder(ctx context.Context, parentSfID, name string) (string, error) {
@@ -53,36 +93,53 @@ func (u *uploader) CreateFolder(ctx context.Context, parentSfID, name string) (s
 		return "", fmt.Errorf("Uploader not initialized")
 	}
 	url := fmt.Sprintf("https://%v/sf/v3/Items(%v)/Folder", u.host, parentSfID)
-	body := bytes.NewReader([]byte(fmt.Sprintf("{\"Name\": \"%v\"}", name)))
-	req, err := http.NewRequest("POST", url, body)
+	toCreate := folder{Name: name}
+	var folder folder
+	err := u.doApiPost(url, &toCreate, &folder)
+	if err != nil {
+		return "", fmt.Errorf("Create folder API call failed: %v", err)
+	}
+	if folder.ID == "" {
+		return "", fmt.Errorf("CreateFolder response did not contain id")
+	}
+	return folder.ID, nil
+}
+
+func (u *uploader) doApiPost(url string, body, expectedResp interface{}) error {
+	bodyBytes, err := json.Marshal(body)
+	if err != nil {
+		return fmt.Errorf("Failed to serialize body: %v", err)
+	}
+	bodyRdr := bytes.NewReader(bodyBytes)
+	req, err := http.NewRequest("POST", url, bodyRdr)
+	if err != nil {
+		return fmt.Errorf("Failed to create httpReq: %v", err)
+	}
 	req.AddCookie(&http.Cookie{Name: authIDcookie, Value: u.authID})
 	req.Header.Add("Content-Type", "application/json")
-	if err != nil {
-		return "", fmt.Errorf("Failed to get createFolder request: %v", err)
-	}
 
 	s, _ := httputil.DumpRequestOut(req, true)
 	fmt.Printf("%v\n", string(s))
 
 	resp, err := u.client.Do(req)
 	if err != nil {
-		return "", fmt.Errorf("createFolder call failed: %v", err)
+		return fmt.Errorf("Request failed: %v", err)
 	}
 	defer resp.Body.Close()
-	bytes, err := ioutil.ReadAll(resp.Body)
+	respBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", fmt.Errorf("Failed to read createFolder response: %v", err)
+		return fmt.Errorf("Failed to read response: %v", err)
 	}
 	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("createFolder api error: %v %v", resp.Status, string(bytes))
+		return fmt.Errorf("Api error: %v %v", resp.Status, string(respBytes))
 	}
-	var folder folder
-	err = json.Unmarshal(bytes, &folder)
+	err = json.Unmarshal(respBytes, expectedResp)
 	if err != nil {
-		return "", fmt.Errorf("Failed to parse createFolder response: %v %v", err, string(bytes))
+		return fmt.Errorf("Failed to parse response: %v %v", err, string(respBytes))
 	}
-	if folder.ID == "" {
-		return "", fmt.Errorf("CreateFolder response did not contain id: %v", string(bytes))
-	}
-	return folder.ID, nil
+	return nil
+}
+
+func (c *Content) reader() io.Reader {
+	return nil
 }
